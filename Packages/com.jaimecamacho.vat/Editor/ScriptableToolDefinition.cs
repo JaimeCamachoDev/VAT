8f5a2568e0b367a9c175dda1d40e9e8462732b43
Jaime Camacho <jaime.camacho.dev@gmail.com>
2025-10-22 15:17:57 +0200

    updated

--- Packages/com.jaimecamacho.vat/Editor/ScriptableToolDefinition.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(menuName = "VAT Tools/Tool Group")]
-public class ScriptableToolGroup : ScriptableObject
-{
-    public List<ToolBase> tools = new List<ToolBase>();
-}
