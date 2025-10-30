a2792b39bef9c0ee3aacb86da4c8fe83e42d3648
Jaime Camacho <jaime.camacho.dev@gmail.com>
2025-10-30 15:12:07 +0100

    Allow VAT Painter mesh filters from assets

--- Packages/com.jaimecamacho.vat/Editor/Tools/VATsToolWindow.cs
+++ Packages/com.jaimecamacho.vat/Editor/Tools/VATsToolWindow.cs
@@ -2054,11 +2054,20 @@ namespace JaimeCamacho.VAT.Editor
             for (int i = 0; i < group.meshFilters.Count; i++)
             {
                 EditorGUILayout.BeginHorizontal();
-                group.meshFilters[i] = (MeshFilter)EditorGUILayout.ObjectField(group.meshFilters[i], typeof(MeshFilter), true);
+                MeshFilter previous = group.meshFilters[i];
+                MeshFilter assigned = (MeshFilter)EditorGUILayout.ObjectField(previous, typeof(MeshFilter), true);
+
+                if (assigned != previous)
+                {
+                    group.meshFilters[i] = assigned;
+                    InvalidatePainterHierarchy(group);
+                }
+
                 if (GUILayout.Button("X", GUILayout.Width(24f)))
                 {
                     group.meshFilters.RemoveAt(i);
                     i--;
+                    InvalidatePainterHierarchy(group);
                 }
                 EditorGUILayout.EndHorizontal();
             }
